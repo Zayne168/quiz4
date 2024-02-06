c6e191da0865f86a87f39188a1c1f092519f0445
Zayne <zayne5504@hotmail.com>
2024-02-05 19:13:10 -0600

    updated dataclass.py

--- quiz4/dataclass.py
+++ quiz4/dataclass.py
@@ -7,3 +7,9 @@ class fish:
         return f"Fish('{self.species}', ${self.price:.2f}, {self.weight}lbs.)"
     def extraMethod(self)->None:
         print(f"I am a {self.species} fish and I weigh {self.weight}lbs! Whee!")
+
+#test material
+myFish=fish("Goldfish", 2.99, 5.5)
+print(myFish)
+myFish.extraMethod()
+#test material
