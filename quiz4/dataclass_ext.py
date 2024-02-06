0ad30ae77d9ebd238120ff36064005b2047b0892
Zayne <zayne5504@hotmail.com>
2024-02-05 19:34:51 -0600

    added dataclass_ext, updated dataclass

--- quiz4/dataclass_ext.py
+++ quiz4/dataclass_ext.py
@@ -5,7 +5,8 @@ class fish:
         self.weight=weight
     def __repr__(self) -> str:
         return f"Fish('{self.species}', ${self.price:.2f}, {self.weight}lbs.)"
-
+    def extraMethod(self)->None:
+        print(f"I am a {self.species} fish and I weigh {self.weight}lbs! Whee!")
 
 #test material
 myFish=fish("Goldfish", 2.99, 5.5)
